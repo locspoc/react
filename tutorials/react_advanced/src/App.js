4151697ad304addd6b7a54797b32acf41108bcec
Loc Tran <mrlocspoc@gmail.com>
2022-03-13 06:20:08 +1030

    react advanced

---
+++ tutorials/react_advanced/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Final from "../../../tutorials/react_advanced/src/tutorial/1-useState/final/5-useState-counter";
+
+function App() {
+  return (
+    <div className="container">
+      <Final />
+    </div>
+  );
+}
+
+export default App;
