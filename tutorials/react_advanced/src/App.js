3457e25b6209d71e71c37675009ea313df30d576
System Administrator <mrlocspoc@gmail.com>
2022-03-19 23:17:47 +1030

    47. useEffect - Basics complete

--- tutorials/react_advanced/src/App.js
+++ tutorials/react_advanced/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-import Final from "../../../tutorials/react_advanced/src/tutorial/1-useState/final/5-useState-counter";
+// import Final from "../../../tutorials/react_advanced/src/tutorial/1-useState/final/5-useState-counter";
+import Setup from "../../../tutorials/react_advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics";
 
 function App() {
   return (
     <div className="container">
-      <Final />
+      {/* <Final /> */}
+      <Setup />
     </div>
   );
 }
