fabec5f202aa301f885547a369b35278d4303f53
Loc Tran <mrlocspoc@gmail.com>
2022-04-30 20:04:22 +0930

    52. Multiple Returns - Basics complete

--- tutorials/react_advanced/src/App.js
+++ tutorials/react_advanced/src/App.js
@@ -4,15 +4,16 @@ import React from "react";
 // import Setup from "../../../tutorials/react_advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics";
 // import Setup from "../../../tutorials/react_advanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup";
 // import Final from "../../../tutorials/react_advanced/src/tutorial/2-useEffect/final/3-useEffect-fetch-data";
-import Setup from "../../../tutorials/react_advanced/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+// import Setup from "../../../tutorials/react_advanced/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+import Setup from "../../../tutorials/react_advanced/src/tutorial/3-conditional-rendering/setup/1-multiple-returns";
 
 function App() {
-  return (
-    <div className="container">
-      {/* <Final /> */}
-      <Setup />
-    </div>
-  );
+	return (
+		<div className="container">
+			{/* <Final /> */}
+			<Setup />
+		</div>
+	);
 }
 
 export default App;
