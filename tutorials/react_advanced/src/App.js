3670ffdf2ff766515c9da4b58d8df613c6ae7b68
Loc Tran <mrlocspoc@gmail.com>
2022-05-07 11:02:29 +0930

    56. Show/Hide Component complete

--- tutorials/react_advanced/src/App.js
+++ tutorials/react_advanced/src/App.js
@@ -6,7 +6,8 @@ import React from "react";
 // import Final from "../../../tutorials/react_advanced/src/tutorial/2-useEffect/final/3-useEffect-fetch-data";
 // import Setup from "../../../tutorials/react_advanced/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data";
 // import Setup from "../../../tutorials/react_advanced/src/tutorial/3-conditional-rendering/setup/1-multiple-returns";
-import Setup from "../../../tutorials/react_advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit";
+// import Setup from "../../../tutorials/react_advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit";
+import Setup from "../../../tutorials/react_advanced/src/tutorial/3-conditional-rendering/setup/3-show-hide";
 
 function App() {
 	return (
