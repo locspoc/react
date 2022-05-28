140600397b7ec996ffdbbbc5f0c8e87ea94ff148
Loc Tran <mrlocspoc@gmail.com>
2022-05-28 13:13:58 +0930

    123. Color Generator - Copy To Clipboard complete

--- projects/09-color-generator/setup/src/SingleColor.js
+++ projects/09-color-generator/setup/src/SingleColor.js
@@ -6,14 +6,25 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
 	const [alert, setAlert] = useState(false);
 	const bcg = rgb.join(",");
 	const hex = rgbToHex(...rgb);
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setAlert(false);
+		}, 3000);
+		return () => clearTimeout(timeout);
+	}, [alert]);
 
 	return (
 		<article
 			className={`color ${index > 10 && "color-light"}`}
 			style={{ backgroundColor: `rgb(${bcg})` }}
+			onClick={() => {
+				setAlert(true);
+				navigator.clipboard.writeText(hex);
+			}}
 		>
 			<p className="percent-value">{weight}%</p>
 			<p className="color-value">{hex}</p>
+			{alert && <p className="alert">copied to clipboard</p>}
 		</article>
 	);
 };
