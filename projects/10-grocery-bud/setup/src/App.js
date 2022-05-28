577d1d23a769d8190e5dfcfab035bdb8bb2e0ef6
Loc Tran <mrlocspoc@gmail.com>
2022-05-28 19:03:15 +0930

    130. Grocery Bud - Edit Item complete

--- projects/10-grocery-bud/setup/src/App.js
+++ projects/10-grocery-bud/setup/src/App.js
@@ -20,7 +20,18 @@ function App() {
 			// setAlert({ show: true, msg: "please enter value", type: "danger" });
 			showAlert(true, "danger", "please enter value");
 		} else if (name && isEditing) {
-			// deal with edit
+			setList(
+				list.map((item) => {
+					if (item.id === editID) {
+						return { ...item, title: name };
+					}
+					return item;
+				})
+			);
+			setName("");
+			setEditID(null);
+			setIsEditing(false);
+			showAlert(true, "success", "value changed");
 		} else {
 			showAlert(true, "success", "item added to the list");
 			const newItem = {
@@ -46,6 +57,13 @@ function App() {
 		setList(list.filter((item) => item.id !== id));
 	};
 
+	const editItem = (id) => {
+		const specificItem = list.find((item) => item.id === id);
+		setIsEditing(true);
+		setEditID(id);
+		setName(specificItem.title);
+	};
+
 	return (
 		<section className="section-center">
 			<form className="grocery-form" onSubmit={handleSubmit}>
@@ -68,7 +86,11 @@ function App() {
 			</form>
 			{list.length > 0 && (
 				<div className="grocery-container">
-					<List items={list} removeItem={removeItem} />
+					<List
+						items={list}
+						removeItem={removeItem}
+						editItem={editItem}
+					/>
 					<button className="clear-btn" onClick={clearList}>
 						clear items
 					</button>
