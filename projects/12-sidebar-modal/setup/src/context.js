031e56e66cbbcf463933657e902b40867ddec780
Loc Tran <mrlocspoc@gmail.com>
2022-07-17 15:53:30 +0930

    142. Sidebar / Modal - Complete

--- projects/12-sidebar-modal/setup/src/context.js
+++ projects/12-sidebar-modal/setup/src/context.js
@@ -3,7 +3,39 @@ import React, { useState, useContext } from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-	return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	// Sidebar
+	const openSidebar = () => {
+		setIsSidebarOpen(true);
+	};
+	const closeSidebar = () => {
+		setIsSidebarOpen(false);
+	};
+
+	// Modal
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
+
+	return (
+		<AppContext.Provider
+			value={{
+				isModalOpen,
+				isSidebarOpen,
+				openModal,
+				openSidebar,
+				closeModal,
+				closeSidebar,
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	);
 };
 
 // custom hook
