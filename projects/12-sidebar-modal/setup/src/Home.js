dd08ab7d95a8ef583ed842c018cc051034662906
Loc Tran <mrlocspoc@gmail.com>
2022-07-17 15:30:57 +0930

    141. Sidebar/Modal - Context complete

--- projects/12-sidebar-modal/setup/src/Home.js
+++ projects/12-sidebar-modal/setup/src/Home.js
@@ -1,8 +1,18 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
+import React, { useContext } from "react";
+import { FaBars } from "react-icons/fa";
+import { AppContext, useGlobalContext } from "./context";
 
 const Home = () => {
-  return <h2>home component</h2>
-}
+	const data = useGlobalContext();
+	console.log(data);
+	return (
+		<main>
+			<button className="sidebar-toggle">
+				<FaBars />
+			</button>
+			<button className="btn">show modal</button>
+		</main>
+	);
+};
 
-export default Home
+export default Home;
