5df31a7a7652771d4c21fc72bf871e25fb211c7c
Loc Tran <mrlocspoc@gmail.com>
2022-07-23 19:42:32 +0930

    72. Custom Hooks - useFetch complete

--- tutorials/react_advanced/src/App.js
+++ tutorials/react_advanced/src/App.js
@@ -13,7 +13,8 @@ import React from "react";
 // import Setup from "../../../tutorials/react_advanced/src/tutorial/5-useRef/setup/1-useRef-basics";
 // import Setup from "../../../tutorials/react_advanced/src/tutorial/6-useReducer/setup";
 // import Setup from "../../../tutorials/react_advanced/src/tutorial/7-prop-drilling/setup/1-prop-drilling";
-import Setup from "../../../tutorials/react_advanced/src/tutorial/8-useContext/setup/1-context-api";
+// import Setup from "../../../tutorials/react_advanced/src/tutorial/8-useContext/setup/1-context-api";
+import Setup from "../../../tutorials/react_advanced/src/tutorial/9-custom-hooks/setup/1-fetch-example";
 
 function App() {
 	return (
