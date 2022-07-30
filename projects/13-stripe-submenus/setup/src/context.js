ba093fe6f0f1057c9955839e728d5bb1b562cdb5
Loc Tran <mrlocspoc@gmail.com>
2022-07-30 19:31:20 +0930

    151. Stripe - Links complete

--- projects/13-stripe-submenus/setup/src/context.js
+++ projects/13-stripe-submenus/setup/src/context.js
@@ -7,6 +7,7 @@ export const AppProvider = ({ children }) => {
 	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 	const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
 	const [location, setLocation] = useState({});
+	const [page, setPage] = useState({ page: "", links: [] });
 
 	// Sidebar
 	const openSidebar = () => {
@@ -18,6 +19,8 @@ export const AppProvider = ({ children }) => {
 
 	// Modal
 	const openSubmenu = (text, coordinates) => {
+		const page = sublinks.find((link) => link.page === text);
+		setPage(page);
 		setLocation(coordinates);
 		setIsSubmenuOpen(true);
 	};
@@ -35,6 +38,7 @@ export const AppProvider = ({ children }) => {
 				closeSubmenu,
 				closeSidebar,
 				location,
+				page,
 			}}
 		>
 			{children}
