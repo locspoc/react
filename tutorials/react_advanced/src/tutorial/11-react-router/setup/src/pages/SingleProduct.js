273ca20bba4972fe4d24a2116bc08e8055158d24
Loc Tran <mrlocspoc@gmail.com>
2022-08-06 23:43:05 +0930

    173. URL Params - Setup complete

--- tutorials/react_advanced/src/tutorial/11-react-router/setup/src/pages/SingleProduct.js
+++ tutorials/react_advanced/src/tutorial/11-react-router/setup/src/pages/SingleProduct.js
@@ -1,9 +1,12 @@
+import { Link, useParams } from "react-router-dom";
+import products from "../data";
 const SingleProduct = () => {
-  return (
-    <section className='section product'>
-      <h2>single product</h2>
-    </section>
-  );
+	const { productId } = useParams();
+	return (
+		<section className="section product">
+			<h4>{productId}</h4>
+		</section>
+	);
 };
 
 export default SingleProduct;
