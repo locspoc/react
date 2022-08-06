e02f137dc1b810f44d481de8caa440df90b37cd0
Loc Tran <mrlocspoc@gmail.com>
2022-08-06 21:06:26 +0930

    169. Nested Routes complete

--- tutorials/react_advanced/src/tutorial/11-react-router/setup/src/App.js
+++ tutorials/react_advanced/src/tutorial/11-react-router/setup/src/App.js
@@ -10,12 +10,16 @@ function App() {
 		<BrowserRouter>
 			<nav>our navbar</nav>
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="about" element={<About />} />
-				<Route path="products" element={<Products />} />
-				<Route path="*" element={<Error />} />
-				<footer>our footer</footer>
+				<Route path="/" element={<Home />}>
+					<Route path="about" element={<About />} />
+					<Route path="products" element={<Products />} />
+					<Route path="*" element={<Error />} />
+				</Route>
+				<Route path="dashboard" element={<div>dashboard</div>}>
+					<Route path="stats" element={<div>stats</div>} />
+				</Route>
 			</Routes>
+			<footer>our footer</footer>
 		</BrowserRouter>
 	);
 }
