32300aa04670c47435663cef1baa5c045d6f9e73
Loc Tran <mrlocspoc@gmail.com>
2022-08-06 23:57:18 +0930

    174. Single Product Page complete

--- tutorials/react_advanced/src/tutorial/11-react-router/setup/src/pages/Products.js
+++ tutorials/react_advanced/src/tutorial/11-react-router/setup/src/pages/Products.js
@@ -1,11 +1,20 @@
+import { Link } from "react-router-dom";
+import products from "../data";
+
 const Products = () => {
-  return (
-    <>
-      <section className='section'>
-        <h2>products</h2>
-      </section>
-    </>
-  );
+	return (
+		<section className="section">
+			<h2>products</h2>
+			{products.map((product) => {
+				return (
+					<article key={product.id}>
+						<h5>{product.name}</h5>
+						<Link to={`/products/${product.id}`}>more info</Link>
+					</article>
+				);
+			})}
+		</section>
+	);
 };
 
 export default Products;
