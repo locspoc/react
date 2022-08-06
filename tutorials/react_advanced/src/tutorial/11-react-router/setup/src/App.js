a5a6af4cfd92bc3799d31cdc2cb8a2a17cd2489a
Loc Tran <mrlocspoc@gmail.com>
2022-08-06 20:58:35 +0930

    168. Navbar complete

--- tutorials/react_advanced/src/tutorial/11-react-router/setup/src/App.js
+++ tutorials/react_advanced/src/tutorial/11-react-router/setup/src/App.js
@@ -8,11 +8,13 @@ import Error from "./pages/Error";
 function App() {
 	return (
 		<BrowserRouter>
+			<nav>our navbar</nav>
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="about" element={<About />} />
 				<Route path="products" element={<Products />} />
 				<Route path="*" element={<Error />} />
+				<footer>our footer</footer>
 			</Routes>
 		</BrowserRouter>
 	);
