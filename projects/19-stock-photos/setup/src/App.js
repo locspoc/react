c61c82941edac182e470b2e325da93dbf097f19f
Loc Tran <mrlocspoc@gmail.com>
2022-09-22 21:13:53 +0930

    223. Stock Photos - Infinite Scroll complete

--- projects/19-stock-photos/setup/src/App.js
+++ projects/19-stock-photos/setup/src/App.js
@@ -10,23 +10,29 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
 	const [loading, setLoading] = useState(false);
 	const [photos, setPhotos] = useState([]);
+	const [page, setPage] = useState(1);
+
 	const fetchImages = async () => {
 		setLoading(true);
 		let url;
-		url = `${mainUrl}${clientID}`;
+		const urlPage = `&page=${page}`;
+		url = `${mainUrl}${clientID}${urlPage}`;
 		try {
 			const response = await fetch(url);
 			const data = await response.json();
-			setPhotos(data);
+			setPhotos((oldPhotos) => {
+				return [...oldPhotos, ...data];
+			});
 			setLoading(false);
 		} catch (error) {
 			setLoading(false);
 			console.log(error);
 		}
 	};
+
 	useEffect(() => {
 		fetchImages();
-	}, []);
+	}, [page]);
 
 	useEffect(() => {
 		const event = window.addEventListener("scroll", () => {
@@ -35,15 +41,19 @@ function App() {
 				window.innerHeight + window.scrollY >=
 					document.body.scrollHeight - 2
 			) {
-				console.log("it worked");
+				setPage((oldPage) => {
+					return oldPage + 1;
+				});
 			}
 		});
 		return () => window.removeEventListener("scroll", event);
 	}, []);
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		console.log("hello");
 	};
+
 	return (
 		<main>
 			<section className="search">
