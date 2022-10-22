77031cdcdfbd300303060c5d55abc10b3b20864e
Loc Tran <mrlocspoc@gmail.com>
2022-10-22 12:51:00 +1030

    248. Hacker News - Basic Structure complete

--- projects/22-hacker-news/setup/src/reducer.js
+++ projects/22-hacker-news/setup/src/reducer.js
@@ -1,10 +1,17 @@
 import {
-  SET_LOADING,
-  SET_STORIES,
-  REMOVE_STORY,
-  HANDLE_PAGE,
-  HANDLE_SEARCH,
-} from './actions'
+	SET_LOADING,
+	SET_STORIES,
+	REMOVE_STORY,
+	HANDLE_PAGE,
+	HANDLE_SEARCH,
+} from "./actions";
 
-const reducer = () => {}
-export default reducer
+const reducer = (state, action) => {
+	switch (action.type) {
+		case SET_LOADING:
+			return { ...state, isLoading: true };
+		default:
+			throw new Error(`no matching "${action.type}" action type`);
+	}
+};
+export default reducer;
