46d272010e016f6a2dfb393484c46774ad938b7d
Loc Tran <mrlocspoc@gmail.com>
2022-10-29 17:54:10 +1030

    265. Quiz - HandleSubmit complete

--- projects/23-quiz/setup/src/context.js
+++ projects/23-quiz/setup/src/context.js
@@ -86,6 +86,9 @@ const AppProvider = ({ children }) => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		const { amount, category, difficulty } = quiz;
+		const url = `${API_ENDPOINT}amount=${amount}&difficulty=${difficulty}&category=${table[category]}&type=multiple`;
+		fetchQuestions(url);
 	};
 
 	const openModal = () => {
